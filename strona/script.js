b63d98c933b60bd4c0ec56c132e25ed77633483d
Ajhosky <80099559+Ajhosky@users.noreply.github.com>
2022-11-15 21:33:28 +0100

    Add files via upload

---
+++ strona/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    {
+        $("header").css("height","100px");
+        $("header").css("width","100%");
+   
+    }
+    {
+        /*$("#content").css("background", "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(40,37,226,1) 0%, rgba(134,134,221,1) 18%, rgba(103,216,238,1) 52%, rgba(40,181,209,1) 94%)");
+        */
+        $("#content").css("float","right");
+        $("#content").css("width","85%");
+        $("#content").css("height","1500px");
+    }
+   
+    { 
+        $("#menu").css("float","left");
+        // $("#menu").css("background-color","grey");
+        $("#menu").css("height","1500px");
+        $("#menu").css("width","15%");
+    }
+    {  
+        $("#aboveImage").css("text-align","center")
+        $("#aboveImage").css("height","200px");
+        $("#aboveImage").css("width","100%");
+    }
+    {  
+        $("#belowImage").css("text-align","center")
+        $("#belowImage").css("height","200px");
+        $("#belowImage").css("width","100%");
+    }
+    {  
+        $("#midleSquare").css("text-align","center")
+        $("#midleSquare").css("height","200px");
+        $("#midleSquare").css("width","100%");
+        {  
+            // $("#leftPanel").css("border","1px solid black")
+            $("#leftPanel").css("text-align","center")
+            $("#leftPanel").css("height","200px");
+            $("#leftPanel").css("width","20%");
+            $("#leftPanel").css("float","left");
+            
+        }
+        {  
+            // $("#rightPanel").css("border","1px solid black")
+            $("#rightPanel").css("text-align","center")
+            $("#rightPanel").css("height","200px");
+            $("#rightPanel").css("width","20%");
+            $("#rightPanel").css("float","left");
+            
+        }
+        {  
+            // $("#image").css("border","1px solid black")
+            $("#image").css("text-align","center")
+            $("#image").css("height","200px");
+            $("#image").css("width","60%");
+            $("#image").css("float","left");
+            
+        }
+    }
+    {
+        $("footer").css("text-align", "center");
+        $("footer").css("padding", "10px");
+    }
+    
+
+});
